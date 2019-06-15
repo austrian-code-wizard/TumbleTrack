d37d730f2afba4bd77de37ffd39d3fb4c08ce439
austrian-code-wizard <moritz.stephan@stanford.edu>
2019-06-15 16:08:10 +0200

    create ultimate gps driver

---
+++ twDevices/ultimateGPS.py
@@ -0,0 +1,76 @@
+from twExceptions.twExceptions import SensorConnectionException
+from time import time
+import asyncio
+import gps
+from twABCs.sensor import Sensor
+
+
+class UltimateGPS(Sensor):
+
+	def __init__(self, controller, timeout=1, host="localhost", port="2947", name="G1"):
+		super().__init__()
+		self._controller = controller
+		controller.register_sensor(self, name)
+		self._timeout = timeout
+		self._run = False
+		self._active = False
+		self._name = name
+		self._device = gps.gps(host, port)
+		self._device.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+		return
+
+	def check(self):
+		"""Start taking temperature measurements. Returns True if the device is
+		initialized, False otherwise.
+		"""
+		return True
+
+	def _measure_value(self):
+		"""Read sensor and return its value in degrees celsius."""
+		# Read temperature register value.
+		data = self._device.next()
+		results = []
+		if data['class'] == 'TPV':
+			if hasattr(data, 'time'):
+				results.append(data.time)
+			else:
+				results.append(None)
+			if hasattr(data, 'lon'):
+				results.append(data.lon)
+			else:
+				results.append(None)
+			if hasattr(data, 'lat'):
+				results.append(data.lat)
+			else:
+				results.append(None)
+			if hasattr(data, 'speed'):
+				results.append(data.speed)
+			else:
+				results.append(None)
+		return results
+
+	def get_single_measurement(self):
+		return self._measure_value()
+
+	async def _measure_continuously(self):
+		loop = asyncio.get_running_loop()
+		self._active = True
+		while self._run:
+			end_time = time() + self._timeout
+			result = await loop.run_in_executor(None, self._measure_value)
+			self._controller.receive_data(str(result), self._name)
+			delta_time = end_time - time()
+			if delta_time > 0:
+				await asyncio.sleep(delta_time)
+		self._active = False
+		return True
+
+	def start(self, loop):
+		self._run = True
+		asyncio.set_event_loop(loop)
+		loop.create_task(self._measure_continuously())
+		return None
+
+	def stop(self):
+		self._run = False
+		return True
