83fb9c94217f5cce944a89a6d41f12a902876169
austrian-code-wizard <moritz.stephan@stanford.edu>
2019-06-20 18:20:48 +0200

    added amg8833 sensor driver

--- twHandler/handlerMethods.py
+++ twHandler/handlerMethods.py
@@ -2,22 +2,25 @@ from twHandler.handler import Handler
 
 handler = Handler()
 
+
 @handler.register_command
 def set_value(object, attribute_name, value):
 	if attribute_name[0] == "_":
 		raise ValueError("Setting of private attributes not allowed")
 	setattr(object, attribute_name, value)
 	return True
-	
+
+
 @handler.register_command
 def set_private_value(object, attribute_name, value):
 	setattr(object, attribute_name, value)
 	return True
 
+
 @handler.register_command
 def confirm_test_message():
 	print("I received a test command!")
-	return True
+
 
 @handler.register_command
 def get_single_measurement(obj):
