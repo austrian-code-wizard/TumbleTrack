d1e51e5c8e6890f8580fe52976f187515bd424d2
Maximilian Podpera <e11825326@student.tuwien.ac.at>
2019-06-28 08:59:16 +0200

    debugging tsl

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 	# gps = UltimateGPS(atmos)
 	ads = ADS1115(atmos)
 	amg = AMG8833(atmos)
-	tsl = TSL2561(atmos)
+	#tsl = TSL2561(atmos)
 	parser.connect(port="/dev/tty.usbmodem141401")
 	parser.run()
 	atmos.run()
