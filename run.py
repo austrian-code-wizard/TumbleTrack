e5ca60d68b91032629e068478c0caed6c78efa2a
austrian-code-wizard <moritz.stephan@stanford.edu>
2019-07-22 00:16:22 +0200

    merging

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from twHandler.handlerMethods import handler
 from twParser.twParser import Parser
 from twModules.atmos import Atmos
 import signal
+from twTesting import device_tests
 from twDevices.mcp9808 import MCP9808
 from twDevices.testDevice import TestDevice
 from twDevices.mpl2115a2 import MPL3115A2
@@ -11,6 +12,7 @@ from twDevices.ultimateGPS import UltimateGPS
 from twDevices.ads1115 import ADS1115
 from twDevices.amg8833 import AMG8833
 from twDevices.tsl2561 import TSL2561
+from twDevices.ccs811 import CCS811
 
 if __name__ == "__main__":
 	try:
@@ -18,23 +20,27 @@ if __name__ == "__main__":
 		handler.register_object(parser, "parser")
 		atmos = Atmos(handler, parser)
 		#handler.register_object(atmos, "atmos")
-		#mcp = MCP9808(atmos)
-		#mpl = MPL3115A2(atmos)
+		# mcp = MCP9808(atmos)
+		# mpl = MPL3115A2(atmos)
 		# htu = HTU21DF(atmos)
-		testdev = TestDevice(atmos)
+		# testdev = TestDevice(atmos)
 		# pms = PMS5003(atmos)
 		# gps = UltimateGPS(atmos)
-		#ads = ADS1115(atmos)
-		#amg = AMG8833(atmos)
-		#tsl = TSL2561(atmos)
-		parser.connect(port="/dev/tty.usbmodem14101")
+		# ads = ADS1115(atmos)
+		# amg = AMG8833(atmos)
+		tsl = TSL2561(atmos)
+		# ccs = CCS811(atmos)
+
+		parser.connect(port="/dev/tty.usbmodem141401")
 		parser.run()
 		atmos.run()
 		signal.pause()
+
+		tsl.check()
 	except KeyboardInterrupt:
 		atmos.stop()
 		parser.stop()
-		print("\n\nThank you for rolling with Team Tumbleweed\n\n")
+		print("Thank you for rolling with Team Tumbleweed\n\n")
 		message = """\
 ████████╗███████╗ █████╗ ███╗   ███╗    ████████╗██╗   ██╗███╗   ███╗██████╗ ██╗     ███████╗██╗    ██╗███████╗███████╗██████╗ 
 ╚══██╔══╝██╔════╝██╔══██╗████╗ ████║    ╚══██╔══╝██║   ██║████╗ ████║██╔══██╗██║     ██╔════╝██║    ██║██╔════╝██╔════╝██╔══██╗
@@ -48,7 +54,4 @@ if __name__ == "__main__":
 ████████╗██╔══██╗██║   ██║██╔══██║██║  ██║   ██║   ██║   ██║██║╚██╔╝██║██╔══██║██╔══██╗╚════██║                                
 ╚██╔═██╔╝██║  ██║╚██████╔╝██║  ██║██████╔╝   ██║   ╚██████╔╝██║ ╚═╝ ██║██║  ██║██║  ██║███████║                                
  ╚═╝ ╚═╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝    ╚═╝    ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝"""
-		print(message)
-
-
-
+print(message)
