3fb4eea1bf5578cbd4490a25918616f4f857e350
Maximilian Podpera <e11825326@student.tuwien.ac.at>
2019-08-04 18:44:16 +0200

    remove work around for filming test sensors to see which are working

--- run.py
+++ run.py
@@ -17,53 +17,35 @@ from twDevices.sht31d import SHT31D
 from twDevices.bno05 import BNO05
 
 
-from time import sleep
 if __name__ == "__main__":
-	try:
-		parser = Parser(handler)  # make sure that the right transceiver device is selected in Parser.__init__()
-		handler.register_object(parser, "parser")
-		atmos = Atmos(handler, parser)
-		handler.register_object(atmos, "atmos")
-		mcp = MCP9808(atmos)
-		# mpl = MPL3115A2(atmos)
-		# htu = HTU21DF(atmos) # TODO HTU READING REGISTER AND METHOD FIX
-		# testdev = TestDevice(atmos)
-		# pms = PMS5003(atmos)
-		# gps = UltimateGPS(atmos)
-		ads = ADS1115(atmos)
-		# amg = AMG8833(atmos)
-		tsl = TSL2561(atmos)
-		# ccs = CCS811(atmos)
-		sht = SHT31D(atmos)
-		# bno = BNO05(atmos)
-		# bno.check()
+    try:
+        parser = Parser(handler)  # make sure that the right transceiver device is selected in Parser.__init__()
+        handler.register_object(parser, "parser")
+        atmos = Atmos(handler, parser)
+        handler.register_object(atmos, "atmos")
+        mcp = MCP9808(atmos)
+        # mpl = MPL3115A2(atmos)
+        # htu = HTU21DF(atmos) # TODO HTU READING REGISTER AND METHOD FIX
+        # testdev = TestDevice(atmos)
+        # pms = PMS5003(atmos)
+        # gps = UltimateGPS(atmos)
+        ads = ADS1115(atmos)
+        # amg = AMG8833(atmos)
+        tsl = TSL2561(atmos)
+        # ccs = CCS811(atmos)
+        sht = SHT31D(atmos)
+        # bno = BNO05(atmos)
+        # bno.check()
 
-
-		#work around for filming Sprinter video
-		run = True
-		while run:
-			sleep(3.0)
-			print("DATA:")
-			sleep(0.2)
-			print("ADS: [" + str(ads.get_single_measurement()[0]) + ", " + str(ads.get_single_measurement()[0]) + ", " + str(ads.get_single_measurement()[0]) + ", " + str(ads.get_single_measurement()[0]) + "]")
-			sleep(0.2)
-			print("TSL(InfraRed): " + str(tsl.get_single_measurement()))
-			sleep(0.2)
-			print("MCP(Temp): " + str(mcp.get_single_measurement()))
-			sleep(0.2)
-			print("SHT(hum): " + str(sht.get_single_measurement()))
-			print("")
-			print("")
-
-		parser.connect(port="/dev/tty.usbmodem141401")
-		parser.run()
-		atmos.run()
-		signal.pause()
-	except KeyboardInterrupt:
-		atmos.stop()
-		parser.stop()
-		print("Thank you for rolling with Team Tumbleweed\n\n")
-		message = """\
+        parser.connect(port="/dev/tty.usbmodem141401")
+        parser.run()
+        atmos.run()
+        signal.pause()
+    except KeyboardInterrupt:
+        atmos.stop()
+        parser.stop()
+        print("Thank you for rolling with Team Tumbleweed\n\n")
+        message = """\
 ████████╗███████╗ █████╗ ███╗   ███╗    ████████╗██╗   ██╗███╗   ███╗██████╗ ██╗     ███████╗██╗    ██╗███████╗███████╗██████╗ 
 ╚══██╔══╝██╔════╝██╔══██╗████╗ ████║    ╚══██╔══╝██║   ██║████╗ ████║██╔══██╗██║     ██╔════╝██║    ██║██╔════╝██╔════╝██╔══██╗
    ██║   █████╗  ███████║██╔████╔██║       ██║   ██║   ██║██╔████╔██║██████╔╝██║     █████╗  ██║ █╗ ██║█████╗  █████╗  ██║  ██║
